375d37159dd1a9182b90538aa20965dfa2a242aa
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-17 22:52:25 +0530

    nurse

--- main.cpp
+++ main.cpp
@@ -19,6 +19,16 @@ int main() {
             cout << "Registering patient..." << endl;
         }
     } while (registered == 'n' || registered == 'N');
+    // Step 3: Keep asking until nurse is available
+    do {
+        cout << "Is a nurse available? (y/n): ";
+        cin >> nurseAvailable;
+ 
+        if (nurseAvailable == 'n' || nurseAvailable == 'N') {
+            cout << "Waiting for nurse availability..." << endl;
+        }
+    } while (nurseAvailable == 'n' || nurseAvailable == 'N');
+
  
     return 0;
 }
